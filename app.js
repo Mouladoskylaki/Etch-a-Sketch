e900823253cc6c83907ac6462cff3aa22b91d908
Konstantinos Adamopoulos <adamssorama@gmail.com>
2024-07-11 21:40:18 +0300

    feat: Create initial 16x16 grid of divs using JavaScript
    - Added HTML structure with a container div.
    - Created a JavaScript function to generate 256
    div elements and append them to the container.
    - Styled the divs with a placeholder text for
    visual confirmation.
    - Linked external CSS and JavaScript files to the
    HTML.
    
    Next steps: Style the grid using Flexbox and
    implement hover effect for the divs.

---
+++ app.js
@@ -0,0 +1,16 @@
+const container = document.querySelector(".container");
+
+const divCreator = () => {
+let divs = [];
+for (let i = 0; i <= 255; i++) {
+    const newDiv = document.createElement("div");
+    newDiv.classList.add("nested-divs")
+    divs.push(newDiv)
+    }
+console.log(divs)
+divs.forEach((item) => {
+    item.textContent = ".";
+    container.appendChild(item);
+})
+}
+divCreator()
