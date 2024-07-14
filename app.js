aa17ae26bf30b15518f3204b939a7a8d46469fa1
Konstantinos Adamopoulos <adamssorama@gmail.com>
2024-07-14 18:21:32 +0300

    Add Rainbow button functionality and implement random color feature
    - Added a new button with the class 'rainbow' to
    the HTML.
    - Implemented a random color generator function.
    - Created a 'rainbow' function that changes the
    background color of grid items to a random color
    on mouseover.
    - Updated the reset function to accommodate
    flexible grid sizes.
    - Added event listener to the Rainbow button to
    trigger the color change functionality.

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 const container = document.querySelector(".container");
+const resetBtn = document.querySelector(".reset");
+const rainbowBtn = document.querySelector(".rainbow");
+
+const randomColor = () => {
+    const randomColor1 = Math.floor(Math.random() * 256);
+    const randomColor2 = Math.floor(Math.random() * 256);
+    const randomColor3 = Math.floor(Math.random() * 256);
+    let finalColor = `rgb(${randomColor1}, ${randomColor2}, ${randomColor3})`;
+    return finalColor;
+}
+
+let divs = [];
 
 const divCreator = () => {
-let divs = [];
 for (let i = 0; i <= 255; i++) {
     const newDiv = document.createElement("div");
     newDiv.classList.add("nested-divs")
@@ -9,13 +20,53 @@ for (let i = 0; i <= 255; i++) {
     }
 console.log(divs)
 divs.forEach((item) => {
-    // item.textContent = ".";
     container.appendChild(item);
     item.addEventListener("mouseover", () => {
-        item.style.backgroundColor = "white";
+        item.style.backgroundColor = "grey";
     })
 })
 }
+
+const reset = () => {
+    while (container.hasChildNodes()) {
+        container.removeChild(container.firstChild)
+    }
+    divs = [];
+    let newBoard = parseInt(prompt("enter a number of squares"));
+    console.log(newBoard);
+    const firstnewBoard = newBoard;
+    newBoard = newBoard * newBoard;
+    for (let i = 1; i <= newBoard; i++) {
+        const newDiv = document.createElement("div");
+        newDiv.classList.add("nested-divs");
+        divs.push(newDiv)
+    }
+    console.log(divs);
+    divs.forEach((item) => {
+        container.appendChild(item);
+        item.style.backgroundColor = "white";
+        item.style.flex = `1 0 calc(100% / ${firstnewBoard})`;
+        item.style.height = `calc(100% / ${firstnewBoard})`;
+        item.addEventListener("mouseover", () => {
+            item.style.backgroundColor = "grey";
+        })
+
+        
+    })
+}
+resetBtn.addEventListener("click", reset);
+
+const rainbow = () => {
+    divs.forEach((item) => {
+        item.addEventListener("mouseover", () => {
+            item.style.backgroundColor = randomColor();
+        })
+    })
+}
+rainbowBtn.addEventListener("click", rainbow);
+
+
 divCreator()
+
 
 
